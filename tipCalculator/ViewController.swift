6599230d654650fe8e2648eca28fcd627a940c18
r3dcrosse <mrdavidwayman@gmail.com>
2015-12-06 21:06:07 -0800

    First Commit
    Remade iOS-tipsCalculator with navigation view controller and a
    responsive layout for any iPhone version.

---
+++ tipCalculator/ViewController.swift
@@ -0,0 +1,52 @@
+//
+//  ViewController.swift
+//  tipCalculator
+//
+//  Created by David Wayman on 11/27/15.
+//  Copyright © 2015 David Wayman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLabel: UILabel!
+    // @IBOutlet weak var SettingsButton: UIBarButtonItem!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view, typically from a nib.
+        tipLabel.text = "$0.00"
+        totalLabel.text = "$0.00"
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // test comment
+    @IBAction func onEditingChange(sender: AnyObject) {
+        let tipPrecentages = [0.15, 0.18, 0.2, 0.22, 0.25]
+        let tipPercentage = tipPrecentages[tipControl.selectedSegmentIndex]
+        
+        let billAmount = NSString(string: billField.text!).doubleValue
+        let tip = billAmount * tipPercentage
+        let total = billAmount + tip
+        
+        tipLabel.text = "$\(tip)"
+        totalLabel.text = "$\(total)"
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
+    
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+}
+
